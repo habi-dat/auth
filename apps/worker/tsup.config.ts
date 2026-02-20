bda9affbd8406b3a9cba9020786d25e8abd19566
Florian Humer <florian.humer@gmail.com>
2026-02-20 17:28:08 +0100

    feat: use tsup to make smaller bundle for worker

---
+++ apps/worker/tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  target: 'node22',
+  platform: 'node',
+  clean: true,
+  noExternal: [/@habidat\/.*/],
+  external: [
+    '@prisma/client',
+    '@prisma/adapter-pg',
+    'pg',
+    'bullmq',
+    'ioredis',
+    'ldapjs-client',
+  ],
+})
